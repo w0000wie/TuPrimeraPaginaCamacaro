abc83b4637dccfc710ad9cc84577801604635658
Jeorgelyz Camacaro <camacarojeorgelyz@gmail.com>
2025-07-05 14:28:30 -0400

    Corrige bloque en base.html y restaura visibilidad de las páginas

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,36 @@ from .models import Post
 def home(request):
     return render(request, 'blog/home.html')
 
+def crear_autor(request):
+    if request.method == 'POST':
+        form = AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = AutorForm()
+    return render(request, 'blog/formulario.html', {'form': form, 'titulo': 'Agregar Autor'})
+
+def crear_categoria(request):
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CategoriaForm()
+    return render(request, 'blog/formulario.html', {'form': form, 'titulo': 'Agregar Categoría'})
+
+def crear_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'blog/formulario.html', {'form': form, 'titulo': 'Agregar Post'})
+
 def buscar_post(request):
     query = request.GET.get('q')
     resultados = []
@@ -15,33 +45,3 @@ def buscar_post(request):
 def detalle_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'blog/detalle_post.html', {'post': post})
-
-def crear_autor(request):
-    if request.method == 'POST':
-        form = AutorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AutorForm()
-    return render(request, 'blog/formulario.html', {'form': form})
-
-def crear_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = PostForm()
-    return render(request, 'blog/formulario.html', {'form': form})
-
-def crear_categoria(request):
-    if request.method == 'POST':
-        form = CategoriaForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = CategoriaForm()
-    return render(request, 'blog/formulario.html', {'form': form})
